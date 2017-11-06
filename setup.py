5926d0279d526998c8cb2918d63d8f2044d33cf3
Jakub Jancicka <jancijak@fit.cvut.cz>
2017-11-06 19:32:27 +0100

    set basic setup

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='labelord_jancijak',
+    version='0.3',
+    description='Management of GitHub labels',
+    author='Jakub Jancicka',
+    author_email='jakub.jancicka@fit.cvut.cz',
+    license='GNU GPLv3',
+    url='https://github.com/jakubjancicka/labelord',
+    py_modules=['labelord'],
+)
